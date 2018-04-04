a404faa1692e681d27008a82d041cf8c85494a88
Alex Martin <martin.alex.t+github@gmail.com>
2018-04-04 01:03:51 -0400

    Add application icon & ensure it's part of the buil script.

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 # Some quick details
 # --out: Output directory
+# --icon: Icon file to use. Extension will be autocompleted based on platform.
 # --overwrite: Always build over the last release.
 # --no-prune: Don't ignore shared dependencies - keep node modules that we need!
+# 
 
-electron-packager . --out=build --overwrite --no-prune
+electron-packager . --out=build --icon=images/GiphySearch-1024 --overwrite --no-prune
